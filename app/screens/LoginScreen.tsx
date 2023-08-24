b58146976149544a21c0ba6a651deda3bf2064aa
blurry <tamsilansari29@gmail.com>
2023-08-24 21:27:21 +0530

    did something

--- app/screens/LoginScreen.tsx
+++ app/screens/LoginScreen.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
 import Typography from '../components/core/Typography';
 import {useTheme} from '../context/theme/themeProvider';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons' 
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import AppTextInput from '../components/core/TextInput';
 
 function LoginScreen() {
   const {colors} = useTheme();
   return (
     <View style={[{backgroundColor: colors.backgroundColor}, styles.container]}>
-      <Icon name={'alien'} size={40} color={'red'}/>
-      <Typography variant="h1" styling={{color: colors.text}}>Login</Typography>
-      <AppTextInput/>
+      <Icon name={'alien'} size={40} color={'red'} />
+      <Typography variant="h1" styling={{color: colors.text}}>
+        Login
+      </Typography>
+      <AppTextInput />
     </View>
   );
 }
