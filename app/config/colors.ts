8313b5b8ceb94b608aa0460ba1dc2a90a0bc380f
blurry <tamsilansari29@gmail.com>
2023-08-24 21:46:17 +0530

    added color science

--- app/config/colors.ts
+++ app/config/colors.ts
@@ -1,22 +1,41 @@
-import {useColorScheme} from 'react-native';
-export interface ColorScheme {
-  primary: string;
-  backgroundColor: string;
-  text: string;
+
+const utilityColors = {
+  success: '#52c41a',
+  danger: '#f5222d',
+  warning: '#faad14',
+  info: '#13c2c2',
 }
 
-const primaryColor = '#22A39F';
+const appThemeColors = {
+  primaryColor: '#22A39F',
+  secondaryColor: '#A32226',
+  light: '#F3EFE0',
+  gray: '#434242',
+  dark: '#222222'
+}
 
-const darkMode: ColorScheme = {
-  primary: primaryColor,
-  backgroundColor: '#222222',
-  text: '#F3EFE0',
+const lightMode = {
+  ...utilityColors,
+  ...appThemeColors,
+  headingText: '#000000E0',
+  text: '#000000E0',
+  secondaryText: '#000000A6',
+  disabledText: '#00000040',
+  border: '#D9D9D9FF',
+  separator: '#0505050F',
+  backgroundColor: '#F5F5F5FF',
 };
 
-const lightMode: ColorScheme = {
-  primary: primaryColor,
-  backgroundColor: '#F3EFE0',
-  text: '#222222',
+const darkMode = {
+  ...utilityColors,
+  ...appThemeColors,
+  headingText: '#FFFFFFD9',
+  text: '#FFFFFFD9',
+  secondaryText: '#FFFFFFA6',
+  disabledText: '#FFFFFF40',
+  border: '#424242FF',
+  separator: '#FDFDFD1F',
+  backgroundColor: '#000000FF',
 };
 
 export {darkMode, lightMode};
