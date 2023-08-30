c5b470e3550f6622eab52cbcf953d6cb59c388ca
blurry <tamsilansari29@gmail.com>
2023-08-30 13:20:47 +0530

    added form and validation

--- src/components/forms/FormField.tsx
+++ src/components/forms/FormField.tsx
@@ -1,4 +1,4 @@
-import React, {ReactNode} from 'react';
+import React, {ReactNode, useMemo} from 'react';
 import {
   View,
   StyleSheet,
@@ -11,6 +11,7 @@ import {
 import {TextInput, Typography} from '../core';
 import ErrorMessage from './ErrorMessage';
 import {useTheme} from '../../context/theme/themeProvider';
+import {Control, useController} from 'react-hook-form';
 
 interface Props {
   preElement?: ReactNode;
@@ -18,6 +19,8 @@ interface Props {
   postElement?: ReactNode;
   textInputStyling?: StyleProp<TextStyle>;
   width?: DimensionValue;
+  name: string;
+  control: Control<any>
 }
 
 function FormField({
@@ -26,15 +29,26 @@ function FormField({
   postElement,
   textInputStyling,
   width = '100%',
+  name,
+  control,
   ...otherProps
 }: TextInputProps & Props) {
   const {colors} = useTheme();
+  const {field, fieldState} = useController({name, control});
 
-  const inputContainer: StyleProp<ViewStyle> = {
-    ...{containerStyling},
-    borderColor: colors.utility.danger,
-    borderWidth: 1,
-  };
+  console.log(name, fieldState)
+
+  const inputContainer: StyleProp<ViewStyle> = useMemo(() => {
+    if (fieldState.invalid) {
+      return {
+        ...{containerStyling},
+        borderColor: colors.utility.danger,
+        borderWidth: 1,
+      };
+    } else {
+      containerStyling;
+    }
+  }, [fieldState]);
 
   return (
     <View style={[styles.container, {width}]}>
@@ -44,8 +58,11 @@ function FormField({
         postElement={postElement}
         textInputStyling={textInputStyling}
         {...otherProps}
+        onChangeText={field.onChange}
+        onBlur={field.onBlur}
+        value={field.value}
       />
-      <ErrorMessage error={'Email must be a valid email!'} visible={true} />
+      <ErrorMessage error={fieldState.error?.message} visible={fieldState.invalid} />
     </View>
   );
 }
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -5,27 +5,53 @@ import {useTheme} from '../context/theme/themeProvider';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {Screen, TextInput, Button} from '../components/core';
 import FormField from '../components/forms/FormField';
+import {useForm} from 'react-hook-form';
+import {ZodType, z} from 'zod';
+import {zodResolver} from '@hookform/resolvers/zod';
+
+type FormData = {
+  email: string;
+  password: string;
+};
 
 function LoginScreen() {
+  const validationSchema: ZodType<FormData> = z.object({
+    email: z.string().email(),
+    password: z.string().min(8),
+  });
+
   const {colors} = useTheme();
   const [isLoading, setIsLoading] = useState(false);
+  const {handleSubmit, control, formState} = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    resolver: zodResolver(validationSchema),
+  });
+
+  console.log(formState.errors)
+
+  const onSubmit = (data: any) => console.log(JSON.stringify(data));
 
   useEffect(() => {
     if (isLoading) setTimeout(() => setIsLoading(false), 2000);
   }, [isLoading]);
+  
   return (
     <Screen style={styles.container}>
       {/* <TextInput placeholder='Email' /> */}
-      <FormField placeholder='Email'/>
-      <TextInput placeholder='Password'/>
+      <FormField placeholder="Email" name={'email'} control={control} />
+      <FormField placeholder="Password" name={'password'} control={control} />
       <Button
         variant="primary"
         title={'Login'}
-        onPress={() => setIsLoading(true)}
+        onPress={handleSubmit(onSubmit)}
         isLoading={isLoading}
-        disabled
       />
-      <Typography variant='b3' styling={{}}>OR</Typography>
+      <Typography variant="b3" styling={{}}>
+        OR
+      </Typography>
       <Button
         variant="primary"
         title={'Sign in with Google'}
@@ -39,7 +65,7 @@ function LoginScreen() {
         title={'Create an account?'}
         onPress={() => setIsLoading(true)}
         isLoading={isLoading}
-        styling={{ position: 'absolute', bottom: 0, left: 0, right: 0}}
+        styling={{position: 'absolute', bottom: 0, left: 0, right: 0}}
       />
     </Screen>
   );
