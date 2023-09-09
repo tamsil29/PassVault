0b7c61eb82ab9da797e00b2846a94c63706dfdda
blurry <tamsilansari29@gmail.com>
2023-09-09 20:10:58 +0530

    added firebase auth

--- App.tsx
+++ App.tsx
@@ -4,9 +4,11 @@ import {StyleSheet} from 'react-native';
 import { ThemeProvider } from '@context/theme/themeProvider';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from '@navigation/AuthNavigator';
+import auth from '@react-native-firebase/auth';
 
 function App(): JSX.Element {
-
+  const user = auth().currentUser;
+  console.log(user)
   return (
     <ThemeProvider>
       <NavigationContainer>
