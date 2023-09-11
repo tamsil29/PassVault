3f47afe01aca4ed59933632d4d1cbc1010a9d676
blurry <tamsilansari29@gmail.com>
2023-09-11 23:51:38 +0530

    wip auth flow

--- App.tsx
+++ App.tsx
@@ -6,23 +6,26 @@ import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from '@navigation/AuthNavigator';
 import auth from '@react-native-firebase/auth';
 import CredetentialListing from '@screens/CredetentialListing';
+import ManagedAuthProvider from '@context/auth/ManagedAuthProvider';
 
 function App(): JSX.Element {
   const user = auth().currentUser;
 
   console.log(user)
 
-  useEffect(() => {
-    auth().onAuthStateChanged(userState => {
-      console.log(userState)
-    });
-  }, []);
+  // useEffect(() => {
+  //   auth().onAuthStateChanged(userState => {
+  //     if(userState)
+  //     console.log(userState)
+  //   });
+  // }, []);
 
   return (
     <ThemeProvider>
       <NavigationContainer>
-        {user ? <CredetentialListing/> : <AuthNavigator/>}
+        {/* {user ? <CredetentialListing/> : <AuthNavigator/>} */}
         {/* <AuthNavigator/> */}
+        <ManagedAuthProvider/>
       </NavigationContainer>
     </ThemeProvider>
   );
---
+++ src/context/auth/ManagedAuthProvider.tsx
@@ -0,0 +1,36 @@
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet} from 'react-native';
+import {AuthProvider, useAuth} from './AuthProvider';
+import CredetentialListing from '@screens/CredetentialListing';
+import AuthNavigator from '@navigation/AuthNavigator';
+import auth from '@react-native-firebase/auth';
+
+function ManagedAuthProvider() {
+  return (
+    <AuthProvider>
+      <AuthManager />
+    </AuthProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default ManagedAuthProvider;
+
+function AuthManager() {
+  //   const {user} = useAuth();
+  const [user, setUser] = useState(null as any);
+
+  useEffect(() => {
+    auth().onAuthStateChanged(userState => {
+      console.log(userState);
+      if (userState) setUser(userState);
+      else setUser(null);
+    });
+  }, []);
+
+  if (user) return <CredetentialListing />;
+  else return <AuthNavigator />;
+}
