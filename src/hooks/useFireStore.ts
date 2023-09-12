4e685bc1edb18ff07ad052549c15395c05219447
blurry <tamsilansari29@gmail.com>
2023-09-12 23:39:58 +0530

    added firebase functions

--- src/hooks/useFireStore.ts
+++ src/hooks/useFireStore.ts
@@ -2,8 +2,9 @@ import React from 'react';
 import firestore from '@react-native-firebase/firestore';
 
 const useFireStore = () => {
-  const cloudFireStore = firestore();
-  return {firestore: cloudFireStore};
+  const firestoreRef = firestore;
+  const cloudFireStore = firestoreRef();
+  return {firestore: cloudFireStore, firestoreRef};
 };
 
 export default useFireStore;
--- src/shared/hooks/useUser.ts
+++ src/shared/hooks/useUser.ts
@@ -2,20 +2,40 @@ import useFireStore from '@hooks/useFireStore';
 import {User} from '@shared/types';
 import React from 'react';
 
-const collection = 'users'
+const collection = 'users';
 
 const useUser = () => {
-  const {firestore} = useFireStore();
+  const {firestore, firestoreRef} = useFireStore();
 
   const setOrUpdateUser = async (user: User) => {
+    await firestore
+      .collection(collection)
+      .doc(user.id)
+      .set({user, createdAt: firestoreRef.FieldValue.serverTimestamp()});
+  };
+
+  const createUser = async (user: User) => {
     await firestore.collection(collection).doc(user.id).set(user);
-  }
+  };
+
+  const updateUser = async (
+    userId: string,
+    userData: Record<string, unknown>,
+  ) => {
+    await firestore
+      .collection(collection)
+      .doc(userId)
+      .update({
+        ...userData,
+        updatedAt: firestoreRef.FieldValue.serverTimestamp(),
+      });
+  };
 
   const getUser = async (user: User) => {
-    return await firestore.collection(collection).doc(user.id).get()
-  }
+    return await firestore.collection(collection).doc(user.id).get();
+  };
 
-  return {setOrUpdateUser, getUser};
+  return {setOrUpdateUser, getUser, updateUser, createUser};
 };
 
 export default useUser;
